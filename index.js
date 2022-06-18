2f4a250e4c17094a53331af917d1ab3f7bffa9ab
Romario <romario.jara.ch@gmail.com>
2022-06-17 19:44:23 -0500

    mongodb GET

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
 const app = express()
+
+const Person = require('./models/person')
 
 app.use(cors())
 
@@ -15,29 +18,6 @@ morgan.token('data', function (req, res) {
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
-
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
 
 const generateId = (min, max) => {
   return Math.ceil(Math.random() * ((max - min) + min))
@@ -59,24 +39,10 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    return response.json(person)
-  } else {
-    return response.status(404).end()
-  }
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -105,7 +71,21 @@ app.post('/api/persons', (request, response) => {
   response.json(newPerson)
 })
 
-const PORT = process.env.PORT || 3001
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      response.json(person)
+    })
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  // const id = Number(request.params.id)
+  // persons = persons.filter(person => person.id !== id)
+
+  // response.status(204).end()
+})
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  console.log(typeof(process.argv.length))
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://romadev:${password}@cluster0.gzb004o.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+  .then(() => console.log('connected'))
+  .catch(err => console.log(err))
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  Person.find({})
+    .then(result => {
+      console.log('phonebook:')
+      result.forEach(person => console.log(person.name, person.number))
+      mongoose.connection.close()
+    })
+} else {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+
+  person.save()
+    .then( () => {
+      console.log(`added ${person.name} number ${person.number} to phonebook`)
+      mongoose.connection.close()
+    })
+    .catch(err => console.log(err))
+}
+
+
+
